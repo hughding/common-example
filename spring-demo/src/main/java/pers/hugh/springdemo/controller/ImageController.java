6b4952875ca8e12dbfb639469d53da3304cd50ad
dxz丁修正 <xzding@ctrip.com>
2017-10-19 15:16:42 +0800

    rename

--- spring-demo/src/main/java/pers/hugh/springdemo/controller/ImageController.java
+++ spring-demo/src/main/java/pers/hugh/springdemo/controller/ImageController.java
@@ -1,13 +1,13 @@
 package pers.hugh.springdemo.controller;
 
 import org.apache.commons.io.IOUtils;
+
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
