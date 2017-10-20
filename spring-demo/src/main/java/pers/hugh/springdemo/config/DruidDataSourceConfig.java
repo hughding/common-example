51d734d3e6aa23a0a7493f9d4e0bc5adfa5aac43
dxz丁修正 <xzding@ctrip.com>
2017-10-20 19:38:08 +0800

    add druid datasource config

---
+++ spring-demo/src/main/java/pers/hugh/springdemo/config/DruidDataSourceConfig.java
@@ -0,0 +1,41 @@
+package pers.hugh.springdemo.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+/**
+ * Created by xzding on 2017/10/19.
+ */
+
+@Configuration
+@EnableTransactionManagement
+public class DruidDataSourceConfig {
+
+    @Bean
+    public DataSource dataSource() {
+
+        DruidDataSource datasource = new DruidDataSource();
+        datasource.setUrl(propertyResolver.getProperty("url"));
+        datasource.setDriverClassName(propertyResolver.getProperty("driver-class-name"));
+        datasource.setUsername(propertyResolver.getProperty("username"));
+        datasource.setPassword(propertyResolver.getProperty("password"));
+        datasource.setInitialSize(Integer.valueOf(propertyResolver.getProperty("initialSize")));
+        datasource.setMinIdle(Integer.valueOf(propertyResolver.getProperty("minIdle")));
+        datasource.setMaxWait(Long.valueOf(propertyResolver.getProperty("maxWait")));
+        datasource.setMaxActive(Integer.valueOf(propertyResolver.getProperty("maxActive")));
+        datasource.setMinEvictableIdleTimeMillis(
+                Long.valueOf(propertyResolver.getProperty("minEvictableIdleTimeMillis")));
+        try {
+            datasource.setFilters("stat,wall");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return datasource;
+    }
+
+}
