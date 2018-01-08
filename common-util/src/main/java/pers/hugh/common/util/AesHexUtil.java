fa8e9f58111b20ffbcd1f3d03d4314d4cb7b5722
hughding <849308919@qq.com>
2018-01-08 19:37:51 +0800

    add aes encrypt util

---
+++ common-util/src/main/java/pers/hugh/common/util/AesHexUtil.java
@@ -0,0 +1,147 @@
+package pers.hugh.common.util;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+/**
+ * AES加密，密文转码为十六进制
+ *
+ * @author xzding
+ * @version 1.0
+ * @since <pre>2018/1/8</pre>
+ */
+public class AesHexUtil {
+    private static final String KEY_ALGORITHM = "AES";
+    /**
+     * 默认的加密算法
+     */
+    private static final String DEFAULT_CIPHER_ALGORITHM = "AES/ECB/PKCS5Padding";
+
+    /**
+     * AES 加密操作
+     *
+     * @param content  待加密内容
+     * @param password 加密密码
+     * @return 返回十六进制转码后的加密数据
+     */
+    public static String encrypt(String content, String password) {
+        try {
+            // 创建密码器
+            Cipher cipher = Cipher.getInstance(DEFAULT_CIPHER_ALGORITHM);
+
+            byte[] byteContent = content.getBytes("utf-8");
+
+            // 初始化为加密模式的密码器
+            cipher.init(Cipher.ENCRYPT_MODE, getSecretKey(password));
+
+            // 加密
+            byte[] result = cipher.doFinal(byteContent);
+
+            //十六进制转码返回
+            return parseByte2HexStr(result);
+        } catch (Exception ex) {
+            return null;
+        }
+    }
+
+    /**
+     * AES 解密操作
+     *
+     * @param content
+     * @param password
+     * @return
+     */
+    public static String decrypt(String content, String password) {
+
+        try {
+            //实例化
+            Cipher cipher = Cipher.getInstance(DEFAULT_CIPHER_ALGORITHM);
+
+            //使用密钥初始化，设置为解密模式
+            cipher.init(Cipher.DECRYPT_MODE, getSecretKey(password));
+
+            //执行操作
+            byte[] result = cipher.doFinal(parseHexStr2Byte(content));
+
+            return new String(result, "utf-8");
+        } catch (Exception ex) {
+            return null;
+        }
+    }
+
+    /**
+     * 生成加密秘钥
+     *
+     * @param password
+     * @return
+     */
+    private static SecretKeySpec getSecretKey(final String password) {
+        //返回生成指定算法密钥生成器的 KeyGenerator 对象
+        KeyGenerator kg = null;
+
+        try {
+            kg = KeyGenerator.getInstance(KEY_ALGORITHM);
+
+            //AES 要求密钥长度为 128
+            kg.init(128, new SecureRandom(password.getBytes()));
+
+            //生成一个密钥
+            SecretKey secretKey = kg.generateKey();
+
+            // 转换为AES专用密钥
+            return new SecretKeySpec(secretKey.getEncoded(), KEY_ALGORITHM);
+        } catch (NoSuchAlgorithmException ex) {
+            return null;
+        }
+    }
+
+    /**
+     * 将二进制转换成16进制
+     *
+     * @param buf
+     * @return
+     */
+    public static String parseByte2HexStr(byte buf[]) {
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < buf.length; i++) {
+            String hex = Integer.toHexString(buf[i] & 0xFF);
+            if (hex.length() == 1) {
+                hex = '0' + hex;
+            }
+            sb.append(hex.toUpperCase());
+        }
+        return sb.toString();
+    }
+
+    /**
+     * 将16进制转换为二进制
+     *
+     * @param hexStr
+     * @return
+     */
+    public static byte[] parseHexStr2Byte(String hexStr) {
+        if (hexStr.length() < 1) {
+            return null;
+        }
+        byte[] result = new byte[hexStr.length() / 2];
+        for (int i = 0; i < hexStr.length() / 2; i++) {
+            int high = Integer.parseInt(hexStr.substring(i * 2, i * 2 + 1), 16);
+            int low = Integer.parseInt(hexStr.substring(i * 2 + 1, i * 2 + 2), 16);
+            result[i] = (byte) (high * 16 + low);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String plaintext = "abcdefghijklmn";
+        System.out.println("明文:" + plaintext);
+        String ciphertext = AesHexUtil.encrypt(plaintext, "123456");
+        System.out.println("密文:" + ciphertext);
+        System.out.println("明文:" + AesHexUtil.decrypt(ciphertext, "123456"));
+    }
+
+}
