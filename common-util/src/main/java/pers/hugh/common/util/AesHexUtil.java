b580adf0b385fe5df79ca85dc3fe7d811934c333
hughding <849308919@qq.com>
2018-01-19 17:49:33 +0800

    opt code of aes utils

--- common-util/src/main/java/pers/hugh/common/util/AesHexUtil.java
+++ common-util/src/main/java/pers/hugh/common/util/AesHexUtil.java
@@ -4,6 +4,7 @@ import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 
@@ -33,7 +34,7 @@ public class AesHexUtil {
             // 创建密码器
             Cipher cipher = Cipher.getInstance(DEFAULT_CIPHER_ALGORITHM);
 
-            byte[] byteContent = content.getBytes("utf-8");
+            byte[] byteContent = content.getBytes(StandardCharsets.UTF_8);
 
             // 初始化为加密模式的密码器
             cipher.init(Cipher.ENCRYPT_MODE, getSecretKey(password));
@@ -80,11 +81,8 @@ public class AesHexUtil {
      * @return
      */
     private static SecretKeySpec getSecretKey(final String password) {
-        //返回生成指定算法密钥生成器的 KeyGenerator 对象
-        KeyGenerator kg = null;
-
         try {
-            kg = KeyGenerator.getInstance(KEY_ALGORITHM);
+            KeyGenerator kg = KeyGenerator.getInstance(KEY_ALGORITHM);
 
             //AES 要求密钥长度为 128
             kg.init(128, new SecureRandom(password.getBytes()));
