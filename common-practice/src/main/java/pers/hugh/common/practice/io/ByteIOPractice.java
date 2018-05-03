375150567cc19136f6b108c09b447348a1c52400
hughding <849308919@qq.com>
2018-05-03 20:24:26 +0800

    add common practice

---
+++ common-practice/src/main/java/pers/hugh/common/practice/io/ByteIOPractice.java
@@ -0,0 +1,41 @@
+package pers.hugh.common.practice.io;
+
+import java.io.*;
+import java.nio.charset.Charset;
+
+/**
+ * @author xzding
+ * @date 2018/5/3
+ */
+public class ByteIOPractice {
+
+    private static final String content = "我是谁？我在那？";
+
+    private static final String FILE_PATH = "common-practice/target/filedemo.txt";
+
+    public static void main(String[] args) {
+        try {
+            OutputStream outputStream = new FileOutputStream(FILE_PATH);
+            outputStream.write(content.getBytes(Charset.forName("UTF-8")));
+            outputStream.close();
+
+
+            InputStream inputStream = new FileInputStream(FILE_PATH);
+            //我字占了3个字节
+            int a = inputStream.read();
+            int b = inputStream.read();
+            int c = inputStream.read();
+            byte[] head = new byte[]{(byte) a, (byte) b, (byte) c};
+            System.out.print(new String(head, Charset.forName("UTF-8")));
+
+            //汉字占用2-4个字节，所以取最小公倍数为缓冲池大小
+            byte[] buffer = new byte[12];
+            int readLength = 0;
+            while ((readLength = inputStream.read(buffer)) != -1) {
+                System.out.print(new String(buffer, 0, readLength, Charset.forName("UTF-8")));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ common-practice/src/main/java/pers/hugh/common/practice/io/CharIOPractice.java
@@ -0,0 +1,33 @@
+package pers.hugh.common.practice.io;
+
+import java.io.*;
+
+/**
+ * @author xzding
+ * @date 2018/5/3
+ */
+public class CharIOPractice {
+    private static final String content = "我是谁？我在那？";
+
+    private static final String FILE_PATH = "common-practice/target/filedemo.txt";
+
+    public static void main(String[] args) {
+        try {
+            OutputStream outputStream = new FileOutputStream(FILE_PATH);
+            Writer writer = new OutputStreamWriter(outputStream);
+            writer.append(content);
+            writer.close();
+
+            InputStream inputStream = new FileInputStream(FILE_PATH);
+            Reader reader = new InputStreamReader(inputStream);
+            char[] buffer = new char[12];
+            int readLength = 0;
+            while ((readLength = reader.read(buffer)) != -1) {
+                System.out.print(String.valueOf(buffer, 0, readLength));
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
